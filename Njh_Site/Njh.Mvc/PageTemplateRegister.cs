09aeacac2a501ed67293926eab8a1dd44b74081b
lars <lmiddleton@reasononeinc.com>
2022-12-23 17:55:40 -0500

    Subpage (Left Nav) Template work in progress - in MVC, added new template and its properties and registered it. Unlike the _LandingPageTemplate the new one is in the MVC Views/Shared/PageTemplates/ folder. TODO add appropriate properties, and update layout per Nick's HTML.

--- Njh_Site/Njh.Mvc/PageTemplateRegister.cs
+++ Njh_Site/Njh.Mvc/PageTemplateRegister.cs
@@ -1,3 +1,9 @@
 ﻿// Copyright (c) NJH. All rights reserved.
 
 // TODO: Register the Kentico page templates (with the RegisterPageTemplate attribute) here
+
+[assembly: Kentico.PageBuilder.Web.Mvc.PageTemplates.RegisterPageTemplate(
+    "NJH.SubpageLeftNavTemplate",
+    "Subpage (Left Nav) Template",
+    typeof(Njh.Mvc.Models.PageTemplateProperties.SubpageLeftNavTemplateProperties),
+    "~/Views/Shared/PageTemplates/SubpageLeftNavTemplate.cshtml")]
