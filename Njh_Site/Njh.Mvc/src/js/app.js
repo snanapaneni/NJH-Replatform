9545150dcc588b9e49c66ac3d87b1df38743294b
nhempsey <nhempsey@reasononeinc.com>
2023-01-05 17:26:13 -0500

    WIP Left Rail Nav

--- Njh_Site/Njh.Mvc/src/js/app.js
+++ Njh_Site/Njh.Mvc/src/js/app.js
@@ -12,7 +12,7 @@ import "../styles/app.scss";
 // import Alert from 'bootstrap/js/dist/alert'
 // import Button from 'bootstrap/js/dist/button'
 // import Carousel from 'bootstrap/js/dist/carousel'
-// import Collapse from 'bootstrap/js/dist/collapse'
+import Collapse from "bootstrap/js/dist/collapse";
 // import Dropdown from 'bootstrap/js/dist/dropdown'
 import Modal from "bootstrap/js/dist/modal";
 // import Modal from 'bootstrap/js/dist/offcanvas'
--- Njh_Site/Njh.Mvc/src/static/components/_config.js
+++ Njh_Site/Njh.Mvc/src/static/components/_config.js
@@ -30,7 +30,7 @@ exports.templates = [
     header: "app-header",
     footer: "app-footer",
     main_insert: "article",
-    main_components: ["subpage-header", "subpage-content", "subpage-aside"],
+    main_components: ["subpage-header", "subpage-aside", "subpage-content"],
     options: {
       h1: "Subpage Left Rail",
     },
