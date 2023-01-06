3af2b37b2edc84e67b654bd6003e7c3a9c750ecb
Dwayne Grant <dgrant@reasononeinc.com>
2023-01-06 21:18:52 +0000

    Merged PR 4945: Main Nav Mobile + Desktop

---
+++ Njh_Shared/Njh.Kernel/Definitions/HeaderNavType.cs
@@ -0,0 +1,18 @@
+﻿namespace Njh.Kernel.Definitions
+{
+    /// <summary>
+    /// Defines the type of the header navigation.
+    /// </summary>
+    public enum HeaderNavType
+    {
+        /// <summary>
+        /// Primary navigation.
+        /// </summary>
+        Primary,
+
+        /// <summary>
+        /// Utility navigation.
+        /// </summary>
+        Utility,
+    }
+}
---
+++ Njh_Shared/Njh.Kernel/Services/IPageService.cs
@@ -0,0 +1,13 @@
+﻿using Njh.Kernel.Kentico.Models.PageTypes;
+
+namespace Njh.Kernel.Services
+{
+    /// <summary>
+    /// Defines the service to work with
+    /// <see cref="PageType_Page"/> documents.
+    /// </summary>
+    public interface IPageService
+        : IDocumentService<PageType_Page>
+    {
+    }
+}
