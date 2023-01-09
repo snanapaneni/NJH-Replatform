07f10d0ca59e6ea3feaffc9e54426a35eef1e15b
Ahmed Khalil <akhalil@reasononeinc.com>
2023-01-09 09:03:21 -0500

    NJHK13-310: physician service

---
+++ Njh_Shared/Njh.Kernel/Services/IPhysicianService.cs
@@ -0,0 +1,12 @@
+﻿using Njh.Kernel.Kentico.Models.CustomTables;
+using Njh.Kernel.Kentico.Models.PageTypes;
+
+namespace Njh.Kernel.Services
+{
+    public interface IPhysicianService
+    {
+        PageType_Physician GetPhysicianItemByGuid(Guid physicianGuid, bool cached=true);
+
+        IEnumerable<PageType_Physician> GetPhysician();
+    }
+}
---
+++ Njh_Shared/Njh.Kernel/Services/PhysicianService..cs
@@ -0,0 +1,82 @@
+﻿using CMS.CustomTables;
+using Njh.Kernel.Constants;
+using Njh.Kernel.Kentico.Models.CustomTables;
+using Njh.Kernel.Models;
+
+namespace Njh.Kernel.Services
+{
+    public class PhysicianService : ServiceBase, IPhysicianService
+    {
+        private readonly ContextConfig contextConfig;
+
+        private readonly ICacheService cacheService;
+
+        /// <summary>
+        /// Initializes a new instance of the
+        /// <see cref="PhysicianService"/> class.
+        /// </summary>
+        /// <param name="contextConfig">
+        /// The context config.
+        /// </param>
+        /// <param name="cacheService">
+        /// The cache service.
+        /// </param>
+        public PhysicianService(
+            ContextConfig contextConfig,
+            ICacheService cacheService)
+        {
+            this.contextConfig = contextConfig ??
+                throw new ArgumentNullException(nameof(contextConfig));
+
+            this.cacheService = cacheService ??
+                throw new ArgumentNullException(nameof(cacheService));
+        }
+        internal CustomTable_SectionColorThemesItem GetPhysicianItemByGuid(Guid physicianGuid)
+        {
+            return CustomTableItemProvider
+              .GetItems<CustomTable_SectionColorThemesItem>()
+              .WithGuid(physicianGuid)
+              .ToList()
+              .FirstOrDefault();
+        }
+        public CustomTable_SectionColorThemesItem GetThemesItemByGuid(Guid themeGuid, bool cache = true)
+        {
+            if (!cache)
+            {
+                return GetThemesItemByGuid(themeGuid);
+            }
+            // Todo: update cache dependency to be on item id
+            var cacheParameters = new CacheParameters
+            {
+                CacheKey = string.Format(
+                   DataCacheKeys.DataSetByKey,
+                   "sectionthemes",
+                   themeGuid.ToString()),
+                IsCultureSpecific = false,
+                IsSiteSpecific = false,
+                CacheDependencies = new List<string>
+                    {
+                        string.Format(
+                            DummyCacheKeys.CustomTableItemsAll,
+                            CustomTable_SectionColorThemesItem.CLASS_NAME),
+                    },
+            };
+
+            var result = this.cacheService.Get(
+                cp => GetThemesItemByGuid(themeGuid), cacheParameters);
+
+            return result;
+            
+        }
+
+        /// <summary>
+        /// Returns all Section Color Themes table items straight from the database (no cache).
+        /// </summary>
+        /// <returns></returns>
+    public IEnumerable<CustomTable_SectionColorThemesItem> GetThemesItems()
+    {
+        return CustomTableItemProvider
+            .GetItems<CustomTable_SectionColorThemesItem>();
+    }
+}
+}
