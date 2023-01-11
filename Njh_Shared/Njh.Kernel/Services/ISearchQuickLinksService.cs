1af21d1c5d0ff52892440b6a0a68e82bfa7bf36e
Ahmed Khalil <akhalil@reasononeinc.com>
2023-01-11 08:23:06 -0500

    NJHK13-284: Add search viewcomponent, services and custom table

---
+++ Njh_Shared/Njh.Kernel/Services/ISearchQuickLinksService.cs
@@ -0,0 +1,8 @@
+﻿using Njh.Kernel.Kentico.Models.CustomTables;
+namespace Njh.Kernel.Services
+{
+    public interface ISearchQuickLinksService
+    {
+      IEnumerable<CustomTable_SearchQuickLinksItem> GetSearchQuickLinks(bool enabled = true, bool cached = true);
+    }
+}
---
+++ Njh_Shared/Njh.Kernel/Services/SearchQuickLinksService.cs
@@ -0,0 +1,82 @@
+﻿using CMS.CustomTables;
+using Njh.Kernel.Constants;
+using Njh.Kernel.Kentico.Models.CustomTables;
+using Njh.Kernel.Models;
+
+namespace Njh.Kernel.Services
+{
+    public class SearchQuickLinksService : ServiceBase, ISearchQuickLinksService
+    {
+        private readonly ContextConfig contextConfig;
+
+        private readonly ICacheService cacheService;
+
+        /// <summary>
+        /// Initializes a new instance of the
+        /// <see cref="SectionThemeService"/> class.
+        /// </summary>
+        /// <param name="contextConfig">
+        /// The context config.
+        /// </param>
+        /// <param name="cacheService">
+        /// The cache service.
+        /// </param>
+        public SearchQuickLinksService(
+            ContextConfig contextConfig,
+            ICacheService cacheService)
+        {
+            this.contextConfig = contextConfig ??
+                throw new ArgumentNullException(nameof(contextConfig));
+
+            this.cacheService = cacheService ??
+                throw new ArgumentNullException(nameof(cacheService));
+        }
+
+        public IEnumerable<CustomTable_SearchQuickLinksItem> GetSearchQuickLinks(bool enabled = true, bool cached = true)
+        {
+            if (!cached)
+            {
+                return GetSearchQuickLinks().Where(i => i.Enabled);
+            }
+            // Todo: update cache dependency to be on item id
+            var cacheParameters = new CacheParameters
+            {
+                CacheKey = string.Format(
+                   DataCacheKeys.DataSetByTableName,
+                   "searchquicklinks",
+                   CustomTable_SearchQuickLinksItem.CLASS_NAME),
+                IsCultureSpecific = false,
+                IsSiteSpecific = false,
+                CacheDependencies = new List<string>
+                    {
+                        string.Format(
+                            DummyCacheKeys.CustomTableItemsAll,
+                            CustomTable_SearchQuickLinksItem.CLASS_NAME),
+                    },
+            };
+
+            var result = this.cacheService.Get(
+                cp => GetSearchQuickLinks()
+                .Where(i => i.Enabled), cacheParameters);
+
+            return result;
+        }
+
+
+        /// <summary>
+        /// Returns all Section Color Themes table items straight from the database (no cache).
+        /// </summary>
+        /// <returns></returns>
+        private IEnumerable<CustomTable_SearchQuickLinksItem> GetSearchQuickLinks()
+        {
+            return CustomTableItemProvider
+                .GetItems<CustomTable_SearchQuickLinksItem>();
+        }
+
+    }
+}
+
+
+
+
+
