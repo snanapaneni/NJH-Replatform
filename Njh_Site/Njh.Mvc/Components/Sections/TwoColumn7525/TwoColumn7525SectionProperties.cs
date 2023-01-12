48635e67838d830d0513967efd85a640b5807484
Ahmed Khalil <akhalil@reasononeinc.com>
2023-01-12 22:40:41 +0000

    Merged PR 4963: NJHK13-696,NJHK13-698 & NJHK13-699
    NJHK13-696,NJHK13-698 & NJHK13-699

--- Njh_Site/Njh.Mvc/Components/Sections/TwoColumn7525/TwoColumn7525SectionProperties.cs
+++ Njh_Site/Njh.Mvc/Components/Sections/TwoColumn7525/TwoColumn7525SectionProperties.cs
@@ -1,8 +1,12 @@
-﻿using Njh.Mvc.Models.SectionProperties;
+﻿using Kentico.Forms.Web.Mvc;
+using Njh.Mvc.Models.SectionProperties;
 
 namespace Njh.Mvc.Components.Sections.TwoColumn7525
 {
     public class TwoColumn7525SectionProperties : SectionPropertiesBase
     {
+        [EditingComponent(CheckBoxComponent.IDENTIFIER, Order = 3, Label = "Swap Columns")]
+        [EditingComponentProperty(nameof(CheckBoxProperties.DefaultValue), false)]
+        public bool SwapColumns { get; set; } = false;
     }
 }
