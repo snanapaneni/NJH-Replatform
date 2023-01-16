b9ece0afe9174b5cfd820750426f9bd395675de2
Nick Hempsey <nhempsey@reasononeinc.com>
2023-01-16 16:21:04 +0000

    Merged PR 4971: Cycle 1 FED bug fixes
    **This PR fixes the following bugs:**
    1. [NJHK13-692](https://reasonone.atlassian.net/browse/NJHK13-692)
    1. [NJHK13-694](https://reasonone.atlassian.net/browse/NJHK13-694)
    1. [NJHK13-697](https://reasonone.atlassian.net/browse/NJHK13-697)
    1. [NJHK13-832](https://reasonone.atlassian.net/browse/NJHK13-832)
    1. [NJHK13-833](https://reasonone.atlassian.net/browse/NJHK13-833)
    1. [NJHK13-834](https://reasonone.atlassian.net/browse/NJHK13-834)
    1. [NJHK13-839](https://reasonone.atlassian.net/browse/NJHK13-839)
    1. [NJHK13-844](https://reasonone.atlassian.net/browse/NJHK13-844)

--- Njh_Site/Njh.Mvc/src/js/app.js
+++ Njh_Site/Njh.Mvc/src/js/app.js
@@ -56,6 +56,7 @@ import App__externalLinkClass from "./utilities/external-link-class";
 import App__pagination from "./utilities/pagination";
 import App__responsiveImage from "./utilities/responsive-image";
 import App__sibling from "./utilities/sibling";
+import App__tables from "./utilities/tables";
 import App__urlToolkit from "./utilities/url-toolkit";
 // Animation
 // import "./animation/fade-up";
@@ -299,6 +300,7 @@ App.utils = {
   responsiveImage: App__responsiveImage,
   // sibling: App__sibling,
   // urlToolkit: App__urlToolkit,
+  tables: App__tables,
 };
 
 App.utils.pagination = App__pagination;
@@ -344,6 +346,8 @@ window.addEventListener("load", function (event) {
   App.utils.adjustBodyTopMargin();
 
   App.utils.externalLinkClass.add();
+
+  App.utils.tables.makeResponsive();
 
   reframe('iframe[src*="youtube"], iframe[src*="vimeo"]');
 
