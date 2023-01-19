112999e8d90b5508aa8ff1ed8ac8d032529444d7
Ahmed Khalil <akhalil@reasononeinc.com>
2023-01-19 18:24:51 -0500

    NJHK13-880: remove custom insert button style button

--- Njh_Site/Njh.Mvc/wwwroot/Content/InlineEditors/froala-configuration.js
+++ Njh_Site/Njh.Mvc/wwwroot/Content/InlineEditors/froala-configuration.js
@@ -3,93 +3,7 @@
     var plugins = (richTextEditor.plugins = richTextEditor.plugins || []);
     var configurations = (richTextEditor.configurations = richTextEditor.configurations || {});
 
-    /*
-     * Plugin: insertHTML_2ColumnPlugin
-     */
-    var insertHTML_2ColumnPlugin = function (FroalaEditor) {
-        FroalaEditor.DefineIcon("twoColIcon", {
-            NAME: "twoColumn",
-            PATH: "M21.75 0H2.25A2.25 2.25 0 0 0 0 2.25v16.5A2.25 2.25 0 0 0 2.25 21h19.5A2.25 2.25 0 0 0 24 18.75V2.25A2.25 2.25 0 0 0 21.75 0Zm-10.5 19.5h-9a.75.75 0 0 1-.75-.75V3h9.75v16.5Zm11.25-.75a.75.75 0 0 1-.75.75h-9V3h9.75v15.75Z",
-        });
-        FroalaEditor.RegisterCommand("insertHTML_2Column", {
-            title: "Insert 2 Column Area",
-            icon: "twoColIcon",
-            focus: true,
-            undo: true,
-            refreshAfterCallback: true,
-            callback: function () {
-                this.html.insert(
-                    '<div class="row"><div class="col col-12 col-lg-6"><p>Column content...</p></div><div class="col col-12 col-lg-6"><p>Column content...</p></div></div><p><br></p>'
-                );
-                this.undo.saveStep();
-            },
-        });
-    };
-    // plugins.push(insertHTML_2ColumnPlugin);
 
-    /*
-     * Plugin: insertHTML_3ColumnPlugin
-     */
-    var insertHTML_3ColumnPlugin = function (FroalaEditor) {
-        FroalaEditor.DefineIcon("threeColIcon", {
-            NAME: "threeColumn",
-            PATH: "M21.75 0H2.25A2.25 2.25 0 0 0 0 2.25v16.5A2.25 2.25 0 0 0 2.25 21h19.5A2.25 2.25 0 0 0 24 18.75V2.25A2.25 2.25 0 0 0 21.75 0ZM7.594 19.5H1.969c-.26 0-.469-.336-.469-.75V3h6.094v16.5Zm14.906-.75c0 .414-.21.75-.469.75h-5.625V3H22.5v15.75ZM9.187 3h5.626v16.5H9.186V3Z",
-        });
-        FroalaEditor.RegisterCommand("insertHTML_3Column", {
-            title: "Insert 3 Column Area",
-            icon: "threeColIcon",
-            focus: true,
-            undo: true,
-            refreshAfterCallback: true,
-            callback: function () {
-                this.html.insert(
-                    '<div class="row"><div class="col col-12 col-lg-4"><p>Column content...</p></div><div class="col col-12 col-lg-4""><p>Column content...</p></div><div class="col col-12 col-lg-4"><p>Column content...</p></div></div><p><br></p>'
-                );
-                this.undo.saveStep();
-            },
-        });
-    };
-    //  plugins.push(insertHTML_3ColumnPlugin);
-
-
-    /*
- * Plugin: insertButtonBlue
- */
-    var insertHTML_ButtonBluePlugin = function (FroalaEditor) {
-        FroalaEditor.DefineIcon("buttonsDropDown", { NAME: 'cog', SVG_KEY: 'cogs' });
-        FroalaEditor.RegisterCommand("insertHTML_Button", {
-            title: "Insert Button",
-            icon: "buttonsDropDown",
-            type: "dropdown",
-            focus: true,
-            undo: true,
-            refreshAfterCallback: true,
-            options: {
-                'btn btn-neutral--900': 'Black Button',
-                'btn btn-neutral--900-outline': 'Black Button - outline',
-                'btn btn-neutral--300': 'Grey button',
-                'btn btn-neutral--300-outline': 'Grey button - outline',
-                'btn btn-neutral--100': 'White Button',
-                'btn btn-neutral--100-outline': 'White Button - outline',
-                'btn btn-blue--700': 'Blue button',
-                'btn btn-blue--700-outline': 'Blue button - Outline',
-                'btn btn-orange--900': 'Orange button',
-                'btn btn-orange--900-outline': 'Orange button - Outline',
-                'btn btn-purple--500': 'Purple button',
-                'btn btn-purple--500-outline': 'Purple button - Outline',
-            },
-            callback: function (cmd, val) {
-                var tempSelectedHtml = this.html.getSelected();
-                var aStr = $(tempSelectedHtml).find('a');
-                if (aStr !== undefined && aStr.length > 0) {
-                    aStr.addClass(val);
-                    this.html.insert(aStr.prop("outerHTML"));
-                    this.undo.saveStep();
-                }
-            },
-        });
-    };
-    plugins.push(insertHTML_ButtonBluePlugin);
     /*
      * Toolbar Configuration
      */
@@ -135,7 +49,6 @@
                 "insertLink",
                 "insertImage",
                 "|",
-                "insertHTML_Button",
                 "inlineClass",
                 "|",
                 "insertHTML_2Column",
