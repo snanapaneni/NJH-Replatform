2ba7668a5c5d9dc9d9b739175b5f38ea63811c8d
Dwayne Grant <dgrant@reasononeinc.com>
2023-02-15 21:06:30 +0000

    Merged PR 5107: NJHK13-1224: bug: Press Release Template missing the Media Resources and Media Contacts sections
    Press Release Template missing the Media Resources and Media Contacts sections

---
+++ Njh_Shared/Njh.Kernel/Extensions/PressReleaseSettingsExtensions.cs
@@ -0,0 +1,26 @@
+﻿using Njh.Kernel.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Njh.Kernel.Extensions
+{
+    public static class PressReleaseSettingsExtensions
+    {
+        public static string GetMediaResources(
+            this ISettingsKeyRepository settingsKeyRepository)
+        {
+            return settingsKeyRepository
+                .GetValue<string>("NJHMediaResources");
+        }
+
+        public static string GetMediaContacts(
+            this ISettingsKeyRepository settingsKeyRepository)
+        {
+            return settingsKeyRepository
+                .GetValue<string>("NJHMediaContacts");
+        }
+    }
+}
--- Njh_Site/Njh.Mvc/Models/PageTemplateProperties/PressReleaseTemplateProperties.cs
+++ Njh_Site/Njh.Mvc/Models/PageTemplateProperties/PressReleaseTemplateProperties.cs
@@ -1,6 +1,28 @@
-﻿namespace Njh.Mvc.Models.PageTemplateProperties
+﻿using CMS.Core;
+using CMS.DataEngine;
+using CMS.Helpers;
+using Njh.Kernel.Extensions;
+using Njh.Kernel.Services;
+
+namespace Njh.Mvc.Models.PageTemplateProperties
 {
     public class PressReleaseTemplateProperties : PageTemplatePropertiesBase
     {
+        private readonly ISettingsKeyRepository settingsKeyRepository;
+
+        public PressReleaseTemplateProperties()
+        {
+            settingsKeyRepository = Service.Resolve<ISettingsKeyRepository>();
+        }
+
+        public string MediaContactsContent => this.settingsKeyRepository.GetMediaContacts();
+
+        public string MediaResourcesContent => this.settingsKeyRepository.GetMediaResources();
+
+        public string MediaContactsContentTitle => ResHelper.GetString("NJH.PressRelease.MediaContactsContentTitle");
+
+        public string MediaResourcesContentTitle => ResHelper.GetString("NJH.PressRelease.MediaResourcesContentTitle");
+
+        public string BoilerPlateRTEDefaultCopy => ResHelper.GetString("NJH.PressRelease.BoilerPlateRTEDefaultCopy");
     }
 }
