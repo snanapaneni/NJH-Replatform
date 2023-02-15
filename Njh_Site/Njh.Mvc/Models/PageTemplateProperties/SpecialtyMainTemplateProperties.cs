63abb521ef2ebf1162d37f3a3a7819817ab1b28e
Dwayne Grant <dgrant@reasononeinc.com>
2023-02-15 18:09:06 +0000

    Merged PR 5108: NJHK13-360: change static text - make an appointment
    change static text - make an appointment

--- Njh_Site/Njh.Mvc/Models/PageTemplateProperties/SpecialtyMainTemplateProperties.cs
+++ Njh_Site/Njh.Mvc/Models/PageTemplateProperties/SpecialtyMainTemplateProperties.cs
@@ -1,6 +1,27 @@
-﻿namespace Njh.Mvc.Models.PageTemplateProperties
+﻿using CMS.Core;
+using CMS.Helpers;
+using Njh.Kernel.Extensions;
+using Njh.Kernel.Services;
+
+namespace Njh.Mvc.Models.PageTemplateProperties
 {
     public class SpecialtyMainTemplateProperties : PageTemplatePropertiesBase
     {
+        private readonly ISettingsKeyRepository settingsKeyRepository;
+
+        public SpecialtyMainTemplateProperties()
+        {
+            settingsKeyRepository = Service.Resolve<ISettingsKeyRepository>();
+        }
+
+        public string MakeAnAppointmentText => this.settingsKeyRepository.GetMakeAnAppointmentText();
+
+        public string MakeAnAppointmentUrl => this.settingsKeyRepository.GetMakeAnAppointmentPage();
+
+        public string GlobalPhoneNumber => this.settingsKeyRepository.GetGlobalPhoneNumber();
+
+        public string GlobalPhoneNumberText => this.settingsKeyRepository.GetGlobalPhoneNumberText();
+
+        public string CallText => ResHelper.GetString("NJH.SpecialtyPage.CallText");
     }
 }
